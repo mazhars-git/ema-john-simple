2ca9ea358feb9f5f847e92fe31c5d72ea775e490
Md. Mazharul Islam <mazharulislam611@gmail.com>
2021-03-28 21:39:51 +0600

    update

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import fakeData from '../../fakeData';
 import { addToDatabaseCart, getDatabaseCart } from '../../utilities/databaseManager';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
@@ -7,20 +6,27 @@ import './Shop.css';
 import { Link } from 'react-router-dom';
 
 const Shop = () => {
-    const first10 = fakeData.slice(0, 10);
-    const [products, setProducts] = useState(first10);
+    const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:4000/products')
+        .then(res => res.json())
+        .then(data => setProducts(data))
+    }, [])
 
     useEffect(() => {
         const savedCart = getDatabaseCart();
         const productKeys = Object.keys(savedCart);
-        const previousCart = productKeys.map(existingKey => {
-            const product = fakeData.find(pd => pd.key === existingKey);
-            product.quantity = savedCart[existingKey];
-            return product;
+        fetch('http://localhost:4000/productByKeys', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json',
+            },
+            body: JSON.stringify(productKeys)
         })
-        setCart(previousCart);
-        console.log(previousCart)
+        .then(res => res.json())
+        .then(data => setCart(data))
     },[])
 
     const handleAddProduct = (product) =>{
